a25b6edc9e02dccf6f873dbbfed079d922e877c9
lucy-per <156499446+lucy-per@users.noreply.github.com>
2024-02-07 14:48:10 +0900

    Update quotes.js

--- js/quotes.js
+++ js/quotes.js
@@ -11,6 +11,8 @@ const quotes = [
     quote: "당신이 할 수 있다고 믿든, 할 수 없다고 믿든, 믿는 대로 될 것이다",
     author: "- 헨리 포드",
   },
+  { quote: "지희야 할 일 끝내고 놀아라",
+    author: "- 전은주", },
 ];
 
 const quote = document.querySelector("#quotes span:first-child");
